4887b3e4e39e5e8eb5ec0689b58448822d7048b8
Oldyier <oldyier@qq.com>
2022-07-13 18:33:20 +0800

    三周之和

--- addsum/15.go
+++ addsum/15.go
@@ -1,6 +1,9 @@
 package addsum
 
-import "fmt"
+import (
+	"fmt"
+	"sort"
+)
 
 /*
  * @lc app=leetcode.cn id=15 lang=golang
@@ -11,22 +14,34 @@ import "fmt"
 // @lc code=start
 func ThreeSum(nums []int) [][]int {
 	var res [][]int
-	for i := 0; i < len(nums); i++ {
-		for j := 1; j < len(nums); j++ {
-			for k := len(nums); k > 1; k-- {
-				if nums[i]+nums[j]+nums[k] == 0 {
-					var re []int
-					re = append(re, nums[i])
-					re = append(re, nums[j])
-					re = append(re, nums[k])
-					// fmt.Printf("re:%v", re)
-					res = append(res, re)
-				}
-			}
-		}
-
+	// 先把数组排序
+	sort.Ints(nums)
+	fmt.Printf("nums : %v\n", nums)
+	var reA []int
+	for i := 0; nums[i] < 0; i++ {
+		reA = append(reA, nums[i])
 	}
-	fmt.Printf("res:%v", res)
+	fmt.Printf("reA:%v\n", reA)
+	reB := []int{}
+	for i := len(nums) - 1; nums[i] >= 0; i-- {
+		reB = append(reB, nums[i])
+	}
+	fmt.Printf("reB %v \n", reB)
+	for i := 0; i < len(reA); i++ {
+		for j := len(reB) - 1; j >= 0; j-- {
+			a := 0
+			if i == 2 {
+				a++
+			}
+			if j == 3 {
+				a++
+			}
+			fmt.Printf("i:%v\t", i)
+			fmt.Printf("j:%v\t", j)
+			fmt.Printf("i+j:%v\n", i+j+a)
+
+		}
+	}
 	return res
 }
 
