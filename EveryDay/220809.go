f84c08af2ae5f3454f25a2f62eae5a08cc788249
Oldyier <oldyier@qq.com>
2022-08-10 18:43:58 +0800

    220810

---
+++ EveryDay/220809.go
@@ -0,0 +1,19 @@
+package EveryDay
+
+import (
+	"fmt"
+	"time"
+)
+
+func Func220809() {
+
+	var m = [...]int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30}
+
+	for i, v := range m {
+		go func() {
+			fmt.Println(i, v)
+		}()
+	}
+
+	time.Sleep(time.Second * 3)
+}
